782ed40d764db8cf1e50c41aca673fa521ae15f1
Zakaria Asad <zasad2587@gmail.com>
2024-04-07 21:13:48 +0300

    Building the markets model

--- app/homePageComponents/CryptoHome.jsx
+++ app/homePageComponents/CryptoHome.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Trending from "./Trending";
+import Markets from "./Markets";
 
 const CryptoHome = () => {
   return (
     <div className="max-w-5xl mx-auto px-2">
       <Trending />
+      <Markets />
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import CryptoHome from "./homePageComponents/CryptoHome";
+import Markets from "./homePageComponents/Markets";
 
 export default function Home() {
   return <CryptoHome />;
