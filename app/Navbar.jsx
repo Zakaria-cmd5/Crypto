9e8832d5f72d7565f893ff218d5e65f3387d60fe
Zakaria Asad <zasad2587@gmail.com>
2024-04-08 17:40:38 +0300

    Build the coins chart

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -1,10 +1,20 @@
+'use client'
+
+import { useRouter } from "next/navigation";
 import { LogoIcon } from "./icons/icons";
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
     <div className="bg-gray-800 text-white h-14 flex items-center">
       <div className="max-w-5xl mx-auto px-2 w-full">
-        <div className="flex items-center gap-1 cursor-pointer">
+        <div
+          className="flex items-center gap-1 cursor-pointer"
+          onClick={() => {
+            router.push("/");
+          }}
+        >
           <LogoIcon />
           <p className="font-semibold">
             <span className="text-yellow-500">C</span>ypto
